724533f3e4d770228d5d8d45930fdfb8bfe45f6c
Raymond Li <hi@raymond.li>
2021-04-06 14:07:27 -0400

    Create autologin.user.js

---
+++ autologin.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         UWaterloo Autologin
+// @namespace    https://raymond.li/
+// @version      0.2
+// @license      GNU GPL v3
+// @description  Automatically signs in via ADFS or CAS to Learn, Quest, Crowdmark, Seashell, etc. You must have some form of password manager that automatically injects your username and password for this script to work.
+// @author       Raymond Li <scripts@raymond.li>
+// @match        https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/SA/?cmd=login&languageCd=ENG&
+// @match        https://student.cs.uwaterloo.ca/~cs136/seashell/
+// @match        https://adfs.uwaterloo.ca/*
+// @match        https://cas.uwaterloo.ca/cas/login*
+// @match        https://app.crowdmark.com/sign-in/waterloo
+// @grant        none
+// @updateURL    https://github.com/Raymo111/uwaterloo-userscripts/raw/master/autologin.user.js
+// @downloadURL  https://github.com/Raymo111/uwaterloo-userscripts/raw/master/autologin.user.js
+// ==/UserScript==
+
+(function() {
+    if (window.location.href === "https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/SA/?cmd=login&languageCd=ENG&") {
+        getIdPLink();
+    } else if (window.location.href.indexOf("https://adfs.uwaterloo.ca") !== -1) {
+        setTimeout(() => {
+            Login.submitLoginRequest();
+        }, 500);
+    } else if (window.location.href === "https://student.cs.uwaterloo.ca/~cs136/seashell/") {
+        setTimeout(() => {
+            document.forms[0].childNodes[11].click();
+        }, 500);
+    } else if (window.location.href.indexOf("https://cas.uwaterloo.ca/cas/login") !== -1) {
+        setTimeout(() => {
+            document.forms.fm1.childNodes[7].childNodes[1].click()
+        }, 500);
+    } else if (window.location.href === "https://app.crowdmark.com/sign-in/waterloo") {
+        window.location.href = document.getElementsByClassName("button--large")[0].href;
+    }
+})();
