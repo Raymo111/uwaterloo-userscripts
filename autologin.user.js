17a3354acd89cadba43479bf7fd97a98cf9aca0a
Raymond Li <hi@raymond.li>
2021-04-10 22:44:56 -0400

    Update autologin.user.js

--- autologin.user.js
+++ autologin.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         UWaterloo Autologin
 // @namespace    https://raymond.li/
-// @version      0.2
+// @version      0.3
 // @license      GNU GPL v3
 // @description  Automatically signs in via ADFS or CAS to Learn, Quest, Crowdmark, Seashell, etc. You must have some form of password manager that automatically injects your username and password for this script to work.
 // @author       Raymond Li <scripts@raymond.li>
 // @match        https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/SA/?cmd=login&languageCd=ENG&
 // @match        https://student.cs.uwaterloo.ca/~cs136/seashell/
+// @match        https://marmoset.student.cs.uwaterloo.ca/?target=%2Fview%2Fcourse.jsp%3FcoursePK%3D47
 // @match        https://adfs.uwaterloo.ca/*
 // @match        https://cas.uwaterloo.ca/cas/login*
 // @match        https://app.crowdmark.com/sign-in/waterloo
@@ -21,16 +22,18 @@
     } else if (window.location.href.indexOf("https://adfs.uwaterloo.ca") !== -1) {
         setTimeout(() => {
             Login.submitLoginRequest();
-        }, 500);
+        }, 1500);
     } else if (window.location.href === "https://student.cs.uwaterloo.ca/~cs136/seashell/") {
         setTimeout(() => {
             document.forms[0].childNodes[11].click();
-        }, 500);
+        }, 1500);
     } else if (window.location.href.indexOf("https://cas.uwaterloo.ca/cas/login") !== -1) {
         setTimeout(() => {
             document.forms.fm1.childNodes[7].childNodes[1].click()
-        }, 500);
+        }, 1500);
     } else if (window.location.href === "https://app.crowdmark.com/sign-in/waterloo") {
         window.location.href = document.getElementsByClassName("button--large")[0].href;
+    } else if (window.location.href === "https://marmoset.student.cs.uwaterloo.ca/?target=%2Fview%2Fcourse.jsp%3FcoursePK%3D47") {
+        document.PerformLogin.childNodes[5].click();
     }
 })();
