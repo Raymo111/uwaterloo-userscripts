bc92e2f11c3e9bcbddc6c5af7a04851ede7e4a0c
Raymond Li <hi@raymond.li>
2021-07-04 01:19:13 -0400

    Update autologin.user.js

--- autologin.user.js
+++ autologin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UWaterloo Autologin
 // @namespace    https://raymond.li/
-// @version      0.3
+// @version      0.4
 // @license      GNU GPL v3
 // @description  Automatically signs in via ADFS or CAS to Learn, Quest, Crowdmark, Seashell, etc. You must have some form of password manager that automatically injects your username and password for this script to work.
 // @author       Raymond Li <scripts@raymond.li>
@@ -21,16 +21,19 @@
         getIdPLink();
     } else if (window.location.href.indexOf("https://adfs.uwaterloo.ca") !== -1) {
         setTimeout(() => {
+            PaginatedNext();
+        }, 2000);
+        setTimeout(() => {
             Login.submitLoginRequest();
-        }, 1500);
+        }, 2000);
     } else if (window.location.href === "https://student.cs.uwaterloo.ca/~cs136/seashell/") {
         setTimeout(() => {
             document.forms[0].childNodes[11].click();
-        }, 1500);
+        }, 2000);
     } else if (window.location.href.indexOf("https://cas.uwaterloo.ca/cas/login") !== -1) {
         setTimeout(() => {
             document.forms.fm1.childNodes[7].childNodes[1].click()
-        }, 1500);
+        }, 2000);
     } else if (window.location.href === "https://app.crowdmark.com/sign-in/waterloo") {
         window.location.href = document.getElementsByClassName("button--large")[0].href;
     } else if (window.location.href === "https://marmoset.student.cs.uwaterloo.ca/?target=%2Fview%2Fcourse.jsp%3FcoursePK%3D47") {
